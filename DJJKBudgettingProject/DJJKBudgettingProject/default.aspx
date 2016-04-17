5b612f828e3fc3f66ed37fc4d3b486763e93e075
John Olano <jjolano@me.com>
2016-04-16 21:17:09 -0400

    Started from the bottom

---
+++ DJJKBudgettingProject/DJJKBudgettingProject/default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/material.Master" CodeFile="default.aspx.cs" Inherits="DJJKBudgettingProject._default" %>
+
+<asp:Content ID="content1" ContentPlaceHolderID="ContentBody" runat="server">
+    <h1>Hello!</h1>
+</asp:Content>
---
+++ DJJKBudgettingProject/DJJKBudgettingProject/material.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace DJJKBudgettingProject
+{
+    public partial class material : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(HttpContext.Current.User.Identity.Name))
+            {
+                // Not logged in.
+                linklogout.Visible = false;
+
+                ((HtmlGenericControl)Page.Master.FindControl("PageBody")).Attributes.Add("onload", "showLoginDialog();");
+            }
+            else
+            {
+                LoginMessage.Visible = false;
+            }
+        }
+    }
+}
